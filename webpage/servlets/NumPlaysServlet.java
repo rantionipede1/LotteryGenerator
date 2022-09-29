908733e10dd6be7cc3544c9287379cf82a1e90bd
jdiamon <112420571+jdiamon@users.noreply.github.com>
2022-09-29 19:43:48 -0400

    Create NumPlaysServlet.java

---
+++ webpage/servlets/NumPlaysServlet.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import jakarta.servlet.*;             
+import jakarta.servlet.http.*;        
+import jakarta.servlet.annotation.*;  
+
+@WebServlet("/NumPlaysServlet")   // Configure the request URL for this servlet (Tomcat 7/Servlet 3.0 upwards)
+public class NumPlaysServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+   @Override
+   public void doGet(HttpServletRequest request, HttpServletResponse response)
+         throws IOException, ServletException {
+	   
+     HttpSession session = request.getSession();
+     String data = (String) request.getParameter("plays");
+	   String returnValue;
+     int numPlays;
+	   
+	   if ((session != null) && (session.getAttribute("plays") == null)) {
+		   session = request.getSession(true);
+		   numPlays = Integer.parseInt(data.trim());	// TO-DO: verify if provided data is in integer format first!
+		   
+		   if (numPlays <= 5) {							          // verify maximum 5 plays.
+			   session.setAttribute("plays", numPlays);
+			   session.setAttribute("playsRemaining", numPlays);
+			   response.setContentType("text/plain");
+			   response.getWriter().write("true");
+		   }
+	   }
+   }
+}
